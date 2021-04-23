2b9392e32edc9714f28d32b4aed8d6c2505bcce8
Bin Xin <rhyzix@gmail.com>
2021-04-23 15:00:49 +0800

    update

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
       type: 'javascript/auto', // fix json type
       loader: 'file-loader',
       options: {
+        esModule: false,
         name: '[name]-[hash:8].[ext]',
       },
     }, {
@@ -46,18 +47,10 @@ module.exports = {
     }),
     new HTMLPlugin({
       template: 'index.html.ejs',
-      // https://github.com/kangax/html-minifier#options-quick-reference
-      minify: {
-        collapseWhitespace: true,
-        removeComments: true,
-      },
     }),
   ],
+  experiments: {
+    topLevelAwait: true,
+  },
 }
 
-// default disable comments for `webpack -p`
-// https://github.com/webpack-contrib/terser-webpack-plugin/blob/master/src/index.js#L46
-Object.defineProperty(require('terser-webpack-plugin').prototype, 'options', {
-  get() { return this._options },
-  set(o) { o.terserOptions.output.comments = false; this._options = o },
-})
