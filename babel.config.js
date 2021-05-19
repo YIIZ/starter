2bac96a457834bb08987cbf0ae761cbbad3bb676
Bin Xin <rhyzix@gmail.com>
2021-05-19 19:03:06 +0800

    browserslist

--- babel.config.js
+++ babel.config.js
@@ -2,6 +2,7 @@
 // https://github.com/babel/babel/blob/main/packages/babel-preset-env/data/shipped-proposals.js
 // https://github.com/babel/babel/blob/master/packages/babel-compat-data/scripts/data/plugin-features.js
 // https://github.com/babel/babel/blob/master/packages/babel-compat-data/data/plugins.json
+// https://github.com/zloirock/core-js/blob/master/packages/core-js-compat/src/data.js
 
 module.exports = {
   // ignore babelrc in node_modules
